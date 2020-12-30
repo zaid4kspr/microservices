72efb23ee311836631b28c97ed80edb403245cbf
Zaid Selmi <selmizaid@gmail.com>
2020-12-30 21:13:51 +0100

    fixs

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
 
   production: false,
   api: {
-    user: 'http://service-auth:3300/api/v1',  
-    serverUrl: 'http://service-auth:3300/api'
+    user: 'http://52.91.15.251:3300/api/v1',  
+    serverUrl: 'http://52.91.15.251:3300/api'
 
   },
 
