77548f3b875c6a001b331ded3bdb2461345d4810
Zaid Selmi <selmizaid@gmail.com>
2020-12-30 21:33:31 +0100

    fixs

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-import { simpleAuthGuardService } from "./authentification/simpleAuthGuard";
+import { MyRouterGuardService } from './authentification/routerGuard';
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthModule),
-      canActivate: [simpleAuthGuardService]
   },
 
   {
     path: 'users',
     loadChildren: () =>
       import('./pages/users/users.module').then((m) => m.UsersModule),
-      canActivate: [simpleAuthGuardService]
    
   },
  
@@ -23,12 +20,11 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () =>
       import('./pages/profile/profile.module').then((m) => m.ProfileModule),
-      canActivate: [simpleAuthGuardService]
      
 
   },
  
-  { path: '', pathMatch: 'full', children: [], canActivate: [simpleAuthGuardService] }
+  { path: '', pathMatch: 'full', children: [], canActivate: [MyRouterGuardService] }
 
 ];
 
