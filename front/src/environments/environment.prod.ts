22896ff5910ebc76ba6c36eb2a65781973cc3edd
Zaid Selmi <selmizaid@gmail.com>
2021-01-17 15:24:03 +0100

    wa7la

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
 
   production: false,
   api: {
-    user: 'http://127.0.0.1/backend/v1',  
-    serverUrl: 'http://127.0.0.1/backend'
+    user: 'http://localhost/backend/v1',  
+    serverUrl: 'http://localhost/backend'
 
   },
 
