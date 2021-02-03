b3a4e0592d039fc2b54ab5068690ee29aa296698
Zaid Selmi <selmizaid@gmail.com>
2021-02-03 18:56:20 +0100

    fixing ip

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
 
   production: false,
   api: {
-    user: 'http://3.80.126.241/backend/v1',  
-    serverUrl: 'http://3.80.126.241/backend'
+    user: 'http://18.212.89.160/backend/v1',  
+    serverUrl: 'http://18.212.89.160/backend'
 
   },
 
